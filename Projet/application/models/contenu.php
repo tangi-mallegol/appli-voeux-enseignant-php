2fc96132f85b75c8567c94e51c2ced22e9fc27d1
mathieu-gouhier <matgo76@gmail.com>
2015-06-04 16:16:41 +0200

    Ajout de fonctions dans contenu.php

--- Projet/application/models/contenu.php
+++ Projet/application/models/contenu.php
@@ -7,21 +7,39 @@
 	        parent::__construct();
 	    }
 
+	    //----------SELECT----------
 		function selectCours($login){
-			return $this->db->get_where('contenu,enseignant', array('enseignant.login' => $login,'contenu.enseignant' => $login))->result();	
+			$sql = "SELECT * FROM contenu WHERE enseignant = ?";
+			return $this->db->query($sql, array($login));
 		}
 
 		function selectTP($login){
-
+			$sql = "SELECT * FROM contenu WHERE type = ? AND enseignant = ?";
+			return $this->db->query($sql, array('TP', $login));
 		}
 
 		function selectTD($login){
-
+			$sql = "SELECT * FROM contenu WHERE type = ? AND enseignant = ?";
+			return $this->db->query($sql, array('TD', $login));
 		}
 
 		function selectCM($login){
+			$sql = "SELECT * FROM contenu WHERE type = ? AND enseignant = ?";
+			return $this->db->query($sql, array('CM', $login));
+		}
 
+		//-----------AJOUT-----------
+		function ajoutContenu($module, $partie, $type, $hed, $enseignant){
+			$sql = "INSERT INTO contenu VALUES(?, ?, ?, ?, ?)";
+			$this->db->query($sql, array($module, $partie, $type, $hed, $enseignant));
+		}
+
+		//-----------CHECK-----------
+		function checkEnseignant($module, $partie){
+			$sql = "SELECT enseignant FROM contenue WHERE module=? AND partie=?";
+			$val = $this->db->query($sql, array($module, $partie));
+
+			return isset($val);
 		}
 	}
-
 ?>
