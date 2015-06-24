283eef66a9ceb590e0c6ca100796b70806d482f4
hugues-adamo <adamo.hugues@gmail.com>
2015-06-24 20:53:00 +0200

    Modification du titre de la page enseignant

--- Projet/application/views/enseignant.php
+++ Projet/application/views/enseignant.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title><?php echo $user[0]["prenom"]." ".$user[0]["nom"] ?></title>
+	<title><?php echo "cours de ".$enseignant?></title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	<script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
